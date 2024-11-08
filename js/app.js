de233ecf41ab5ecfed9b05b39e6c403d18518ab7
odrya-keri-mako <odry.attila@keri.mako.hu>
2024-11-08 10:57:18 +0100

    First look

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
-angular.module('app', []);
+;(function(window, angular) {
+
+  'use strict';
+
+  // Application module
+  angular.module('app', [
+    'ui.router'
+  ])
+
+  // Application config
+  .config([
+    '$stateProvider', 
+    '$urlRouterProvider', 
+    function($stateProvider, $urlRouterProvider) {
+
+      $stateProvider
+			.state('home', {
+				url: '/',
+				templateUrl: './html/home.html'
+			})
+			.state('page1', {
+				url: '/page1',
+				templateUrl: './html/page1.html'
+			})
+			.state('page2', {
+				url: '/page2',
+				templateUrl: './html/page2.html'
+			});
+      
+      $urlRouterProvider.otherwise('/');
+    }
+  ]);
+
+
+})(window, angular);
